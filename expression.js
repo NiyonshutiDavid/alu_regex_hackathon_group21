70d6e88d505ec5599244aee442c136062d95ae1d
Jumulisa <144101506+Jumulisa@users.noreply.github.com>
2024-02-12 17:05:18 +0200

    Update expression.js

--- expression.js
+++ expression.js
@@ -1,4 +1,4 @@
-// Regular expression patterns
+
 const movieTitlePattern = /[\w\s]+ \(\d{4}\)/g;
 const songLyricsPattern = /\[Verse \d+\] .*/g;
 const twitterUsernamePattern = /@[\w\d]+/g;
@@ -9,7 +9,6 @@ const datePattern = /\d{2}-[A-Za-z]{3}-\d{4}/g;
 const hexColorPattern = /#[A-Fa-f\d]{6}/g;
 const ipAddressPattern = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/g;
 
-// Extracting data using regular expressions
 const movieTitles = stringResponse.match(movieTitlePattern);
 const songLyrics = stringResponse.match(songLyricsPattern);
 const twitterUsernames = stringResponse.match(twitterUsernamePattern);
@@ -20,7 +19,6 @@ const dates = stringResponse.match(datePattern);
 const hexColors = stringResponse.match(hexColorPattern);
 const ipAddresses = stringResponse.match(ipAddressPattern);
 
-// Output extracted data
 console.log("Movie Titles:", movieTitles);
 console.log("Song Lyrics:", songLyrics);
 console.log("Twitter Usernames:", twitterUsernames);
